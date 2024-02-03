30c17f1f9e21cc56f9ba841e8e7db8248bc577b5
AmauryLna <amau.72+github2@gmail.com>
2024-02-03 16:40:24 +0100

    add player life bar

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
+signal life_changed(new_value: int)
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var file = 100
 
 
 func _physics_process(delta):
