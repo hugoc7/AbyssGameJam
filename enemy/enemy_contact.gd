bd4f5bea4db9a13ef4e0f9cfad634f84024b79f9
hugo <caroubalos.hugo@gmail.com>
2024-02-03 16:11:44 +0100

    camera follow player

--- enemy/enemy_contact.gd
+++ enemy/enemy_contact.gd
@@ -1,11 +1,11 @@
 @tool
-extends CharacterBody2D
+extends Area2D
 
 class_name EnemyContact
 
 @export var color = Enums.LightColor.WHITE
-@export var white_texture : Texture2D
-@export var black_texture : Texture2D
+@export var white_texture : Texture2D = null
+@export var black_texture : Texture2D = null
 
 func _ready():
 	if color == Enums.LightColor.WHITE:
@@ -15,5 +15,6 @@ func _ready():
 	
 func _on_background_color_changed(bkg_color: Enums.LightColor):
 	$Sprite2D.visible = bkg_color != color
+	
 	
 
--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,6 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
