6ce801028f12541914727b9af6eb34cb3450e24e
hugo <caroubalos.hugo@gmail.com>
2024-02-04 17:42:52 +0100

    integrate zombie anim

--- enemy/contact/enemy_contact.gd
+++ enemy/contact/enemy_contact.gd
@@ -15,32 +15,40 @@ class_name EnemyContact
 @export var die_vfx : PackedScene
 
 var is_in_cooldown = false
+var is_dying = false
+var color_as_text = "white" 
 @onready var sprite : AnimatedSprite2D = $Sprite
 
 func take_damage(damage: int):
+	set_animation("hit")
 	life -= damage
 	if life < 0:
 		life = 0
 		die()
 		
 func die():
+	set_animation("death")
 	var die_vfx_instance = die_vfx.instantiate()
 	die_vfx_instance.position = position
 	get_parent().add_child(die_vfx_instance)
-	queue_free()
+	is_dying = true	
+
+func set_animation(anim: String):
+	sprite.animation = anim + "_" + color_as_text 
+		
 
 func _ready():
 	if color == Enums.LightColor.WHITE:
-		sprite.animation = "idle_white"
+		color_as_text = "white"
 	elif color == Enums.LightColor.BLACK:
-		sprite.animation = "idle_black"
+		color_as_text = "black"
+	set_animation("idle")
 	
 	if Engine.is_editor_hint():
 		set_process(false)
 		return
 	
-	sprite.play()
-	body_entered.connect(_on_body_entered)
+	sprite.play(sprite.animation)
 	$CooldownTimer.timeout.connect(_on_cooldown_timeout)
 		
 	
@@ -49,7 +57,7 @@ func _on_background_color_changed(bkg_color: Enums.LightColor):
 	sprite.visible = bkg_color != color
 	
 func _process(delta):
-	if player == null:
+	if player == null or is_dying:
 		return
 	var sqr_dist_to_player = global_position.distance_squared_to(player.position)
 	if sqr_dist_to_player < purchase_distance.y * purchase_distance.y:
@@ -64,15 +72,16 @@ func _process(delta):
 		$CooldownTimer.start()
 
 func _damage_player():
+	set_animation("atk")
 	if player == null:
 		return
 	player.take_damage(damage)
-	print_debug("atk player")
 
 func _on_cooldown_timeout():
 	is_in_cooldown = false
-
-func _on_body_entered(body):
-	pass
-	#_damage_player()
 	
+func _on_sprite_animation_loop():
+	if is_dying:
+		queue_free()
+	else:
+		set_animation("idle")
