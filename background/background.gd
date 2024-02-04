a9d60c7875efc00056f0b52f04afb73575c95855
AmauryLna <amau.72+github2@gmail.com>
2024-02-04 15:10:49 +0100

    parallax backgrounds

--- background/background.gd
+++ background/background.gd
@@ -3,14 +3,14 @@ extends Sprite2D
 
 
 @export var default_color = Enums.LightColor.WHITE
-@export var dark_texture : Texture2D
-@export var light_texture : Texture2D
+@export var black_texture : Texture2D
+@export var white_texture : Texture2D
 
 func _ready():
 	update_color(default_color)
 		
 func update_color(color: Enums.LightColor):
 	if color == Enums.LightColor.WHITE:
-		set_texture(light_texture)
+		set_texture(white_texture)
 	elif color == Enums.LightColor.BLACK:
-		set_texture(dark_texture)
+		set_texture(black_texture)
