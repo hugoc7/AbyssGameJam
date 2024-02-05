77bf7715c057edaee064702928dd57c4c4894083
hugo <caroubalos.hugo@gmail.com>
2024-02-05 23:16:27 +0100

    add victory game over screens

---
+++ menu/victory.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+func _on_quit_button_down():
+	$Quit/Node2D/fond.play()
+	$Quit/Node2D/text.play()
+	
+
+func _on_quit_pressed():
+	get_tree().change_scene_to_file("res://menu/main_menu.tscn")
